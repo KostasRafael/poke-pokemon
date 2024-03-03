f303bb07c294e4ac9df0168d1efb7505076a3b65
KostasRafael <korafamanou@gmail.com>
2024-03-03 12:06:13 +0200

    Declared pokemonList variable.
    I declared pokemonList variable which stores 6 different pokemons and the respective name, type, abilities and height for each one of them.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert('Hello world');
-let favoriteFood = 'lentil soup';
-document.write(favoriteFood);
+let pokemonList = [{name: 'butterfree', type: ['bug','flying'], abilities: ['compoundeyes','tinted-lens'], height: 1.1, },{name: 'bulbasaur', type: ['grass','poison'], abilities: ['chlorophyll','overgrow'], height: 0.7, }, {name: 'charmander',  type: 'fire', abilities: ['blaze', 'solar-power'], height: 0.6,},{name: 'ninetales', type: 'fire', abilities: ['flash-fire','drought'], height: 1.1, },{name: 'pikachu', type: 'electric', abilities: ['static','lightningrod'], height: 0.4, },{name: 'squirtle', type: 'water', abilities: ['rain-dish','torrent'], height: 0.5, }];
