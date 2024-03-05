4f49bca8f17c01f7feab5652b7b139154422da51
KostasRafael <korafamanou@gmail.com>
2024-03-05 16:52:57 +0200

    Added 'for' loop and conditional 'if'.
    I did this to document.write all pokemons with their respective height, and also to document.write a different message for the pokemons that are biger than 1 and can fly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-let pokemonList = [{name: 'butterfree', type: ['bug','flying'], abilities: ['compoundeyes','tinted-lens'], height: 1.1, },{name: 'bulbasaur', type: ['grass','poison'], abilities: ['chlorophyll','overgrow'], height: 0.7, }, {name: 'charmander',  type: 'fire', abilities: ['blaze', 'solar-power'], height: 0.6,},{name: 'ninetales', type: 'fire', abilities: ['flash-fire','drought'], height: 1.1, },{name: 'pikachu', type: 'electric', abilities: ['static','lightningrod'], height: 0.4, },{name: 'squirtle', type: 'water', abilities: ['rain-dish','torrent'], height: 0.5, }];
+let pokemonList = [{name: 'butterfree', type: ['bug','flying'], abilities: ['compoundeyes','tinted-lens'], height: 1.1, },
+                   {name: 'bulbasaur', type: ['grass','poison'], abilities: ['chlorophyll','overgrow'], height: 0.7, },
+                   {name: 'charmander',  type: 'fire', abilities: ['blaze', 'solar-power'], height: 0.6,},
+                   {name: 'ninetales', type: 'fire', abilities: ['flash-fire','drought'], height: 1.1, },
+                   {name: 'pikachu', type: 'electric', abilities: ['static','lightningrod'], height: 0.4, },
+                   {name: 'squirtle', type: 'water', abilities: ['rain-dish','torrent'], height: 0.5, },
+                ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    let types = Array.isArray(pokemonList[i].type) ? pokemonList[i].type: [pokemonList[i].type];
+    if (pokemonList[i].height > 1 && types.includes('flying')) {
+    document.write('<h1>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' -' + ' Wow, that\'s a big pokemon and can also fly!' + '</h1>');
+    } else {
+        document.write('<h1>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '</h1>');
+    }
+}
+
