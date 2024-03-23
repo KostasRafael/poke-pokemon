c6f3d1e4ec1240888210c7e6d6dac1b122df1f11
KostasRafael <korafamanou@gmail.com>
2024-03-23 12:10:13 +0200

    Called add () function to add another pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,76 @@
-let pokemonRepository = (function() {
-    
-    let pokemonList = [
-      {name: 'butterfree', type: ['bug','flying'], abilities: ['compoundeyes','tinted-lens'], height: 1.1, },
-      {name: 'bulbasaur', type: ['grass','poison'], abilities: ['chlorophyll','overgrow'], height: 0.7, },
-      {name: 'charmander',  type: 'fire', abilities: ['blaze', 'solar-power'], height: 0.6,},
-      {name: 'ninetales', type: 'fire', abilities: ['flash-fire','drought'], height: 1.1, },
-      {name: 'pikachu', type: 'electric', abilities: ['static','lightningrod'], height: 0.4, },
-      {name: 'squirtle', type: 'water', abilities: ['rain-dish','torrent'], height: 0.5, },
- ];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "butterfree",
+      type: ["bug", "flying"],
+      abilities: ["compoundeyes", "tinted-lens"],
+      height: 1.1,
+    },
+    {
+      name: "bulbasaur",
+      type: ["grass", "poison"],
+      abilities: ["chlorophyll", "overgrow"],
+      height: 0.7,
+    },
+    {
+      name: "charmander",
+      type: "fire",
+      abilities: ["blaze", "solar-power"],
+      height: 0.6,
+    },
+    {
+      name: "ninetales",
+      type: "fire",
+      abilities: ["flash-fire", "drought"],
+      height: 1.1,
+    },
+    {
+      name: "pikachu",
+      type: "electric",
+      abilities: ["static", "lightningrod"],
+      height: 0.4,
+    },
+    {
+      name: "squirtle",
+      type: "water",
+      abilities: ["rain-dish", "torrent"],
+      height: 0.5,
+    },
+  ];
 
- function getAll() {
+  function getAll() {
     return pokemonList;
- }
+  }
 
- function add(item) {
+  function add(item) {
     pokemonList.push(item);
- }
+  }
 
 
- return {
+  return {
     getAll: getAll,
-    add: add
- }
+    add: add,
+  };
 
- 
-
+  pokemonRepository.add(
+   {
+      name: 'Chaos',
+      type: 'grass',
+      abilities: 'snore',
+      height: 5,
+   }
+  );
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-    console.log(pokemon.name + ' is a ' + pokemon.type + ' pokemon, and has the abilities of ' + pokemon.abilities + ' a height of ' + pokemon.height + '.')
-})
-
-
-
-
+pokemonRepository.getAll().forEach(function (pokemon) {
+  console.log(
+    pokemon.name +
+      " is a " +
+      pokemon.type +
+      " pokemon, and has the abilities of " +
+      pokemon.abilities +
+      " a height of " +
+      pokemon.height +
+      "."
+  );
+});
